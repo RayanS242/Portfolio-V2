0025e28ae07c0b72f1816caa480ff0aa080f7733
RayanS242 <rayan.spall02@gmail.com>
2024-06-21 01:08:49 +0200

    installed project + created header
    .

---
+++ my-app/src/lib/components/Header.svelte
@@ -0,0 +1,64 @@
+<header>
+    <div class="header-logo">
+        <h1>R</h1>
+        <div>
+            <p>Rayan</p>
+            <p>Web Developer</p>
+        </div>
+    </div>
+    <div class="header-navigation">
+        <a href="Home">Home</a>
+        <a href="Info">Info</a>
+    </div>
+    <div class="header-socials">
+        <a href="GitHub">GitHub</a>
+        <a href="Linkedin">Linkedin</a>
+    </div>
+</header>
+
+
+
+<style>
+
+header {
+    display: flex;
+    justify-content: space-between;
+    margin: 1rem;
+}
+
+.header-logo {
+    display: flex;
+    flex-direction: row;
+    
+}
+
+h1 {
+    color: white;
+    font-size: 50px;
+}
+
+p {
+    color: white;
+    transform: translateY(80%);
+}
+
+.header-navigation {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    background-color: rgb(36, 35, 35);
+    border-radius: 15px;
+    padding: 1rem;
+}
+
+div a {
+    text-decoration: none;
+    color: white;
+    transform: translateY(10%);
+}
+
+.header-socials {
+    transform: translateY(30%);
+}
+
+</style>
